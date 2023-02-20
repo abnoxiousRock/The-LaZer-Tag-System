d436dfbeefa11941fc413ca7742d5d2bb9af2873
Joshua Rose <jlr007@protonmail.com>
2023-02-19 18:53:46 -0600

    Add new player and get players API calls working

--- server-root/app.js
+++ server-root/app.js
@@ -1,6 +1,12 @@
-const express = require("express");
+const express = require('express');
 const dgram = require('dgram');
 const classes = require('./classes');
+const {createClient} = require('@supabase/supabase-js');
+const { json } = require('express');
+
+const SUPA_URL = 'https://hkgckknlshbgvedsphzs.supabase.co';
+const SUPA_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhrZ2Nra25sc2hiZ3ZlZHNwaHpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY0MDI3MDUsImV4cCI6MTk5MTk3ODcwNX0.9zWMlf9DqDfP4zuKOz4oLW80GPypJfogOJIqw1N79hM';
+const sbClient = createClient(SUPA_URL, SUPA_KEY);
 
 const app = express();
 app.use(express.static('public'));
@@ -36,10 +42,40 @@ app.get("/scores", function (req, res) {
     res.end(JSON.stringify(scoreboard));
 });
 
-app.post("/", function(req, res) {
+app.get("/dbdata", function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    (async () => {
+            let {data, error} = await sbClient.from('player').select();
+            if (error)
+                res.end(JSON.stringify(error));
+            else
+                res.end(JSON.stringify(data));
+    })();
+});
+
+app.post("/register", function (req, res) {
+    console.log("Post received to add user to database");
+    console.log(req.body);
+    res.setHeader('Content-Type', 'application/json');
+    (async () => {
+            let {data, error} = await sbClient.from('player').insert({
+                id: req.body.data.id,
+                first_name: req.body.data.fName,
+                last_name: req.body.data.lName,
+                codename: req.body.data.nName
+            });
+            if (error)
+                res.end(JSON.stringify(error));
+            else
+                res.end(JSON.stringify(data));
+    })();
+
+    res.send();
+});
+
+app.post("/", function (req, res) {
     console.log("Post received, time to start game");
     console.log(req.body);
-    //need to send success response message or just use 200 okay code?
     res.send();
 });
 
--- server-root/public/index.js
+++ server-root/public/index.js
@@ -8,10 +8,12 @@ var fadeSplash = function() {
 window.onload = function() {
     setTimeout(fadeSplash, 3000);
     let form = document.getElementById('myForm');
-    form.addEventListener('submit', handleForm);
+    form.addEventListener('submit', handleFormStartGame);
+    form = document.getElementById('regPlayer');
+    form.addEventListener('submit', handleFormRegister);
 }
 
-function handleForm(event) {
+function handleFormStartGame(event) {
     event.preventDefault();
     startGame();
     //function to disable form/button and prevent further changes and 
@@ -21,6 +23,31 @@ function handleForm(event) {
     //function hide player entry form, unhide scoreboard with hit action ticker
     //function to start timer?
     //conditions for a win to stop game early?
+}
+
+function handleFormRegister(event) {
+    event.preventDefault();
+    register();
+}
+
+let register = function () {
+    let newId = document.getElementById('newId').value;
+    let fName = document.getElementById('fName').value;
+    let lName = document.getElementById('lName').value;
+    let nName = document.getElementById('nName').value;
+    let returnObj = {
+        'id': newId,
+        'fName': fName,
+        'lName': lName,
+        'nName': nName,
+    }
+
+    let post = JSON.stringify({'data': returnObj});
+    const url = '/register'
+    let xhr = new XMLHttpRequest()
+    xhr.open('POST', url, true)
+    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+    xhr.send(post);
 }
 
 let startGame = function () {
