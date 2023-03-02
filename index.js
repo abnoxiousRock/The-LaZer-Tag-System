52fff08ed733cf4d5a67aa1b8b36e9a47cf3b9d7
Joshua Rose <jlr007@protonmail.com>
2023-03-02 14:37:18 -0600

    deleting static html and other unneeded stuff

--- index.js
+++
@@ -1,8 +0,0 @@
-var fadeSplash = function() {
-    const elem = document.getElementById('splashscreen');
-    elem.style.display = 'none';
-}
-
-window.onload = function() {
-    setTimeout(fadeSplash, 3000);
-  }
